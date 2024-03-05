8514032fea7c68ad071c62604d45427a236a3366
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2024-03-05 18:01:31 +0530

    docker compose file added

--- src/app/initServer.ts
+++ src/app/initServer.ts
@@ -5,14 +5,9 @@ import bodyParser from 'body-parser';
 import { ApolloServer } from '@apollo/server';
 import {expressMiddleware} from '@apollo/server/express4'
 
-// import { User } from "@dinecraft/backend/packages/restaurant/graphql/user"
 import { User } from '../packages/restaurant/graphql/user';
-// import { Cuisine } from '../packages/restaurant/graphql/cuisine';
 import { Employee } from '../packages/restaurant/graphql/employee';
-// import { Category } from '../packages/restaurant/graphql/category';
 import AuthRoutes  from '../packages/restaurant/routes/restaurant.route';
-// import CuisineRoutes from '../packages/restaurant/routes/cuisine.route';
-// import CategoryRoutes from '../packages/restaurant/routes/category.route';
 import EmployeeRoutes from '../packages/restaurant/routes/employee.route';
 
 export async function initServer(){
@@ -21,8 +16,6 @@ export async function initServer(){
     app.use(cors());
 
     app.use('/restaurant', AuthRoutes);
-    // app.use('/restaurant', CuisineRoutes);
-    // app.use('/restaurant', CategoryRoutes);
     app.use('/restaurant', EmployeeRoutes);
     
     const server = new ApolloServer({
