7cd3907cee1d3734305d6c6733977ed8fcf8b553
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2024-03-28 23:07:32 +0530

    formatted file

--- src/packages/restaurant/controllers/employee.controller.ts
+++ src/packages/restaurant/controllers/employee.controller.ts
@@ -1,93 +1,102 @@
 import Employee from "../../../db/restaurants/employee.model";
 
-export const addEmployee = async ({restaurant_id ,firstName, lastName, phone, emailId, password, role, address, isVerified, isBlocked}:{
-    firstName: string,
-    lastName: string,
-    restaurant_id: string,
-    phone: string,
-    emailId: string,
-    password: string,
-    role: string,
-    address: string,
-    isVerified: boolean,
-    isBlocked: boolean
+export const addEmployee = async ({
+  restaurant_id,
+  firstName,
+  lastName,
+  phone,
+  emailId,
+  password,
+  role,
+  address,
+  isVerified,
+  isBlocked,
+}: {
+  firstName: string;
+  lastName: string;
+  restaurant_id: string;
+  phone: string;
+  emailId: string;
+  password: string;
+  role: string;
+  address: string;
+  isVerified: boolean;
+  isBlocked: boolean;
 }) => {
-    try {
+  try {
+    const employee = await Employee.create({
+      firstName: firstName,
+      lastName: lastName,
+      restaurantId: restaurant_id,
+      phone: phone,
+      email: emailId,
+      password: password,
+      role: role,
+      address: address,
+      isVerified: isVerified,
+      isBlocked: isBlocked,
+    });
+    return employee;
+  } catch (error) {
+    throw error;
+  }
+};
 
-        const employee = await Employee.create({
-            firstName: firstName,
-            lastName: lastName,
-            restaurantId: restaurant_id,
-            phone: phone,
-            email: emailId,
-            password: password,
-            role: role,
-            address: address,
-            isVerified: isVerified,
-            isBlocked: isBlocked
-        });
-        return employee;
-    } catch (error) {
-        throw error;
-    }
-}
-
-
-export const getEmployeesByRestaurant = async ({RestaurantId}:{
-    RestaurantId: string
+export const getEmployeesByRestaurant = async ({
+  RestaurantId,
+}: {
+  RestaurantId: string;
 }) => {
-    try {
-        const employees = await Employee.findAll({
-            where: {
-                restaurantId: RestaurantId
-            }
-        });
-        return employees;
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const employees = await Employee.findAll({
+      where: {
+        restaurantId: RestaurantId,
+      },
+    });
+    return employees;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const getEmployeeByRestaurant = async ({employeeId}:{
-    employeeId: string
+export const getEmployeeByRestaurant = async ({
+  employeeId,
+}: {
+  employeeId: string;
 }) => {
-    try {
-        const employee = await Employee.findOne({
-            where: {
-                employeeId
-            }
-        });
-        if (!employee) {
-            throw new Error('Employee not found');
-        }
-        return employee;
-    } catch (error) {
-        throw error;
+  try {
+    const employee = await Employee.findOne({
+      where: {
+        employeeId,
+      },
+    });
+    if (!employee) {
+      throw new Error("Employee not found");
     }
-}
+    return employee;
+  } catch (error) {
+    throw error;
+  }
+};
 
-
-
-
-
-export const deleteEmployeeByRestaurant = async ({employeeId}:{
-    employeeId: string
+export const deleteEmployeeByRestaurant = async ({
+  employeeId,
+}: {
+  employeeId: string;
 }) => {
-    try {
-        const employee = await Employee.findOne({
-            where: {
-                employeeId
-            }
-        });
-        if (!employee) {
-            throw new Error('Employee not found');
-        }
-        employee.setDataValue('isDeleted', true);
-        await employee.save();
-        return employee;
-    } catch (error) {
-        throw error;
+  try {
+    const employee = await Employee.findOne({
+      where: {
+        employeeId,
+      },
+    });
+    if (!employee) {
+      throw new Error("Employee not found");
     }
-}
-
-
+    employee.setDataValue("isDeleted", true);
+    await employee.save();
+    return employee;
+  } catch (error) {
+    throw error;
+  }
+};
--- src/packages/restaurant/routes/employee.route.ts
+++ src/packages/restaurant/routes/employee.route.ts
@@ -1,31 +1,34 @@
-import express from 'express'
+import express from "express";
 
-import { addEmployee, getEmployeesByRestaurant } from '../controllers/employee.controller'
+import {
+  addEmployee,
+  getEmployeesByRestaurant,
+} from "../controllers/employee.controller";
 
-import { verifyToken } from '../middlewares/verifyToken';
+import { verifyToken } from "../middlewares/verifyToken";
 
 const router = express.Router();
 
-router.get('/:restaurant_id/employees', verifyToken , async (req, res) => {
-    try{
-        const {restaurant_id} = req.params;
-        const employees = await getEmployeesByRestaurant({RestaurantId: restaurant_id});
-        res.status(200).send(employees);
-    }
-    catch (error) {
-        res.status(400).send(error);
-    }
+router.get("/:restaurant_id/employees", verifyToken, async (req, res) => {
+  try {
+    const { restaurant_id } = req.params;
+    const employees = await getEmployeesByRestaurant({
+      RestaurantId: restaurant_id,
+    });
+    res.status(200).send(employees);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-router.post('/:restaurant_id/employees', async (req, res) => {
-    try {
-        const {restaurant_id} = req.params;
-        const employee = await addEmployee({restaurant_id, ...req.body});
-        res.status(200).send(employee)
-    } catch (error) {
-        res.status(400).send
-    }
-})
-
+router.post("/:restaurant_id/employees", async (req, res) => {
+  try {
+    const { restaurant_id } = req.params;
+    const employee = await addEmployee({ restaurant_id, ...req.body });
+    res.status(200).send(employee);
+  } catch (error) {
+    res.status(400).send;
+  }
+});
 
 export default router;
